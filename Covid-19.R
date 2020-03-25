af48073ae2702357c731c3187c28d03519e257fa
bas <bas@anubis.local>
2020-03-25 11:13:32 +0100

    - read csv from ecdc - changes to layout of plots

--- Covid-19.R
+++ Covid-19.R
@@ -3,11 +3,19 @@ library(httr)
 library(tidyverse)
 library(cowplot)
 
-#url <- "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-23.xlsx"
-url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
-GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".xlsx")))
-data <- read_excel(tf)
-Population <- read_csv("population-figures-by-country.csv")
+EcdcUrl <- "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-24.csv"
+#EcdcUrl <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
+
+data <- read_csv(url(EcdcUrl),
+                 col_types = cols(Cases = col_integer(),
+                 DateRep = col_date(format = "%d/%m/%Y"),
+                 Day = col_integer(), Deaths = col_integer(),
+                 Month = col_integer(), Year = col_integer()))
+
+Population <- read_csv(url("https://datahub.io/JohnSnowLabs/population-figures-by-country/r/population-figures-by-country-csv.csv"))
+
+
+
 
 Countries <- c("AT","DE","ES","FR","IT","UK","US")
 PopCountries <- c("AUT","DEU","ESP","FRA","ITA","GBR","USA") # WHY HAVE STANDARDS?!
@@ -15,14 +23,14 @@ PopCountries <- c("AUT","DEU","ESP","FRA","ITA","GBR","USA") # WHY HAVE STANDARD
 Population <- Population %>%
   filter (Country_Code %in% PopCountries) %>%
   select(Country_Code, ends_with("2016")) %>%
-  mutate(Country_Code = countries) %>%
+  mutate(Country_Code = Countries) %>%
   rename(GeoId = Country_Code, Population100k = Year_2016) %>%
   mutate(Population100k = Population100k/100000)
 
 DataSelected <- data %>%
   arrange(DateRep) %>%
   filter (Month >= "3") %>%
-  filter (GeoId %in% countries) %>%
+  filter (GeoId %in% Countries) %>%
   inner_join(Population) %>%
   group_by(GeoId) %>%
   mutate(CumCases = cumsum(Cases)) %>%
@@ -37,7 +45,9 @@ DataSelected <- data %>%
 CasesByCountry <- ggplot(data = DataSelected, aes(x = DateRep)) +
   geom_line(aes (y = Cases, color = GeoId)) +
   labs(title = "Daily Cases", x = NULL, y = NULL) +
-  theme(legend.position = "none", axis.text.x = element_blank())
+  labs(colour = "Countries") +
+  theme(legend.justification=c(0,1), legend.position=c(0.01,0.99),
+        axis.text.x = element_blank())
 
 CumCasesByCountry <- ggplot(data = DataSelected, aes(x = DateRep)) +
   geom_line(aes (y = CumCases, color = GeoId)) +
@@ -47,7 +57,7 @@ CumCasesByCountry <- ggplot(data = DataSelected, aes(x = DateRep)) +
 CumCasesPerPop <- ggplot(data = DataSelected, aes(x = DateRep)) +
   geom_line(aes (y = CumCasesPer100k, color = GeoId)) +
   labs(title = "Cumulative Cases per 100k Inhabitants", x = NULL, y = NULL) +
-  labs(colour = "Countries", axis.text.x = element_blank())
+  theme(legend.position = "none", axis.text.x = element_blank())
 
 DeathsByCountry <- ggplot(data = DataSelected, aes(x = DateRep)) +
   geom_line(aes (y = Deaths, color = GeoId)) +
@@ -62,7 +72,7 @@ CumDeathsByCountry <- ggplot(data = DataSelected, aes(x = DateRep)) +
 CumDeathsPerPop <- ggplot(data = DataSelected, aes(x = DateRep)) +
   geom_line(aes (y = CumDeathsPer100k, color = GeoId)) +
   labs(title = "Cumulative Deaths per 100k Inhabitants", x = NULL, y = NULL) +
-  labs(colour = "Countries", caption = "(based on data from www.ecdc.europa.eu)")
+  theme(legend.position = "none")
 
 plot_grid(CasesByCountry, CumCasesByCountry, CumCasesPerPop,
           DeathsByCountry, CumDeathsByCountry, CumDeathsPerPop,
