f08e09f02f8ef83cd34c69f275d94120e74cd517
bas <bas@anubis.local>
2020-04-03 14:56:14 +0200

    code cleaning, comments, changes to plot-layout

--- Covid-19.R
+++ Covid-19.R
@@ -1,8 +1,8 @@
 library(httr)
 library(tidyverse)
 library(cowplot)
-#library(directlabels)
 
+#Retrieve Data from ECDC
 GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate(":", ":", type="ntlm"),
     write_disk(tf <- tempfile(fileext = ".csv")))
 
@@ -12,8 +12,11 @@ data <- read_csv(tf,
                                   day = col_integer(), deaths = col_integer(),
                                   month = col_integer(), year = col_integer()))
 
+# Set Countries to include in Plots:
 Countries <- c("AT","DE","ES","FR","IT","UK","US")
 
+
+# Filter data for plotting and create cumulative and per 100k variables:
 DataSelected <- data %>%
   arrange(dateRep) %>%
   filter (month >= "3") %>%
@@ -28,28 +31,36 @@ DataSelected <- data %>%
   mutate(CumCasesPer100k = CumCases/Population100k) %>%
   mutate(CumDeathsPer100k = CumDeaths/Population100k)
 
+
+# Create plots:
 CasesByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
   geom_line(aes (y = cases, color = geoId)) +
   labs(title = "Daily cases", x = NULL, y = NULL) +
   labs(colour = "Countries") +
   scale_x_date(date_breaks = "5 days",
                date_minor_breaks = "1 days") +
+  scale_y_continuous(labels = scales::unit_format(
+    unit = "k",
+    scale = 1e-3)) +
   theme(legend.justification=c(0,1), legend.position=c(0.01,0.99),
         axis.text.x = element_blank())
 
 CumCasesByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
   geom_line(aes (y = CumCases, color = geoId)) +
-  scale_y_log10() +
   scale_x_date(date_breaks = "5 days",
                date_minor_breaks = "1 days") +
-  # geom_dl(aes(y=CumCases,label=geoId),method="top.qp") +
-  labs(title = "Cumulative cases (log)", x = NULL, y = NULL) +
+  scale_y_continuous(labels = scales::unit_format(
+    unit = "k",
+    scale = 1e-3,
+    accuracy = 0.01)) +
+  labs(title = "Cumulative cases", x = NULL, y = NULL) +
   theme(legend.position = "none", axis.text.x = element_blank())
 
 CumCasesPerPop <- ggplot(data = DataSelected, aes(x = dateRep)) +
   geom_line(aes (y = CumCasesPer100k, color = geoId)) +
   scale_x_date(date_breaks = "5 days",
                date_minor_breaks = "1 days") +
+  scale_y_continuous() +
   labs(title = "Cumulative cases per 100k Inhabitants", x = NULL, y = NULL) +
   theme(legend.position = "none", axis.text.x = element_blank())
 
@@ -59,15 +70,18 @@ DeathsByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
   scale_x_date(date_labels = "%b %d",
                date_breaks = "5 days",
                date_minor_breaks = "1 days") +
+  scale_y_continuous() +
   theme(legend.position = "none")
 
 CumDeathsByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
   geom_line(aes (y = CumDeaths, color = geoId)) +
-  scale_y_log10() +
   scale_x_date(date_labels = "%b %d",
                date_breaks = "5 days",
                date_minor_breaks = "1 days") +
-  labs(title = "Cumulative deaths (log)", x = NULL, y = NULL) +
+  scale_y_continuous(labels = scales::unit_format(
+    unit = "k",
+    scale = 1e-3)) +
+  labs(title = "Cumulative deaths", x = NULL, y = NULL) +
   theme(legend.position = "none")
 
 CumDeathsPerPop <- ggplot(data = DataSelected, aes(x = dateRep)) +
@@ -75,53 +89,13 @@ CumDeathsPerPop <- ggplot(data = DataSelected, aes(x = dateRep)) +
   scale_x_date(date_labels = "%b %d",
                date_breaks = "5 days",
                date_minor_breaks = "1 days") +
+  scale_y_continuous() +
   labs(title = "Cumulative deaths per 100k Inhabitants", x = NULL, y = NULL) +
   theme(legend.position = "none")
 
+
+# Combine plots:
 plot_grid(CasesByCountry, CumCasesByCountry, CumCasesPerPop,
           DeathsByCountry, CumDeathsByCountry, CumDeathsPerPop,
           labels = "AUTO")
-
-
-# Plots in log Scale:
-
-# CasesByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = cases, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Daily cases (log Scale)", x = NULL, y = NULL) +
-#   theme(legend.position = "none", axis.text.x = element_blank())
-#
-# CumCasesByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = CumCases, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Cumulative cases (log Scale)", x = NULL, y = NULL) +
-#   theme(legend.position = "none", axis.text.x = element_blank())
-#
-# CumCasesPerPop <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = CumCasesPer100k, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Cumulative cases per 100k Inhabitants (log Scale)", x = NULL, y = NULL) +
-#   labs(colour = "Countries", axis.text.x = element_blank())
-#
-# DeathsByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = deaths, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Daily deaths (log Scale)", x = NULL, y = NULL) +
-#   theme(legend.position = "none")
-#
-# CumDeathsByCountry <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = CumDeaths, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Cumulative deaths (log Scale)", x = NULL, y = NULL) +
-#   theme(legend.position = "none")
-#
-# CumDeathsPerPop <- ggplot(data = DataSelected, aes(x = dateRep)) +
-#   geom_line(aes (y = CumDeathsPer100k, color = geoId)) +
-#   scale_y_log10() +
-#   labs(title = "Cumulative deaths per 100k Inhabitants (log Scale)", x = NULL, y = NULL) +
-#   labs(colour = "Countries", caption = "(based on data from www.ecdc.europa.eu)")
-#
-# plot_grid(CasesByCountry, CumCasesByCountry, CumCasesPerPop,
-#           DeathsByCountry, CumDeathsByCountry, CumDeathsPerPop,
-#           labels = "AUTO")
 
